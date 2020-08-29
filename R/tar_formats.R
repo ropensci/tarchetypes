287520ae9556c82cebb57d775feefdda990a2a63
wlandau <will.landau@gmail.com>
2020-08-29 12:53:46 -0400

    Fix #10

--- R/tar_formats.R
+++ R/tar_formats.R
@@ -18,3 +18,77 @@
 #' targets::tar_make()
 #' }
 NULL
+
+tar_format <- function(format) {
+  out <- tar_format_api
+  body(out) <- substitute({
+    envir <- tar_option_get("envir")
+    targets::tar_target_raw(
+      name = deparse_language(substitute(name)),
+      command = tidy_eval(substitute(command), envir, tidy_eval),
+      pattern = pattern,
+      packages = packages,
+      library = library,
+      format = format,
+      iteration = iteration,
+      error = error,
+      memory = memory,
+      deployment = deployment,
+      priority = priority,
+      template = template,
+      resources = resources,
+      storage = storage,
+      retrieval = retrieval,
+      cue = cue
+    )
+  }, env = list(format = format))
+  out
+}
+
+tar_format_api <- function(
+  name,
+  command,
+  pattern = NULL,
+  tidy_eval = targets::tar_option_get("tidy_eval"),
+  packages = targets::tar_option_get("packages"),
+  library = targets::tar_option_get("library"),
+  iteration = targets::tar_option_get("iteration"),
+  error = targets::tar_option_get("error"),
+  memory = targets::tar_option_get("memory"),
+  deployment = targets::tar_option_get("deployment"),
+  priority = targets::tar_option_get("priority"),
+  template = targets::tar_option_get("template"),
+  resources = targets::tar_option_get("resources"),
+  storage = targets::tar_option_get("storage"),
+  retrieval = targets::tar_option_get("retrieval"),
+  cue = targets::tar_option_get("cue")
+) {
+}
+
+#' @export
+#' @rdname tar_formats
+tar_file <- tar_format("file")
+
+#' @export
+#' @rdname tar_formats
+tar_rds <- tar_format("rds")
+
+#' @export
+#' @rdname tar_formats
+tar_qs <- tar_format("qs")
+
+#' @export
+#' @rdname tar_formats
+tar_fst <- tar_format("fst")
+
+#' @export
+#' @rdname tar_formats
+tar_fst_dt <- tar_format("fst_dt")
+
+#' @export
+#' @rdname tar_formats
+tar_fst_tbl <- tar_format("fst_tbl")
+
+#' @export
+#' @rdname tar_formats
+tar_keras <- tar_format("keras")
--- tests/testthat/test-tar_formats.R
+++ tests/testthat/test-tar_formats.R
@@ -1,5 +1,52 @@
+tar_test("tar_file()", {
+  x <- tar_file(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "file")
+})
+
 tar_test("tar_rds()", {
-  x <- tar_rds(x, 1)
+  x <- tar_rds(x, 0)
   expect_true(inherits(x, "tar_target"))
   expect_equal(x$settings$format, "rds")
+})
+
+tar_test("tar_qs()", {
+  x <- tar_qs(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "qs")
+})
+
+tar_test("tar_fst()", {
+  x <- tar_fst(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "fst")
+})
+
+tar_test("tar_fst_dt()", {
+  x <- tar_fst_dt(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "fst_dt")
+})
+
+tar_test("tar_fst_tbl()", {
+  x <- tar_fst_tbl(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "fst_tbl")
+})
+
+tar_test("tar_keras()", {
+  x <- tar_keras(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "keras")
+})
+
+tar_test("tar_format_api()", {
+  expect_null(tar_format_api(x, 1))
+})
+
+tar_test("tar_format()", {
+  f <- tar_format("file")
+  x <- f(x, 0)
+  expect_true(inherits(x, "tar_target"))
+  expect_equal(x$settings$format, "file")
 })
