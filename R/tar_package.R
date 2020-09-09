014aa091e545dacb539e8f543f60b77eb4e34098
wlandau <will.landau@gmail.com>
2020-09-09 12:25:17 -0400

    Declare global variables

--- R/tar_package.R
+++ R/tar_package.R
@@ -12,6 +12,9 @@
 #'   tar_pipeline tar_read tar_script tar_target tar_target_raw
 #' @importFrom tidyselect all_of any_of contains ends_with everything
 #'   last_col matches num_range one_of starts_with
+#' @importFrom utils globalVariables
 #' @importFrom vctrs vec_c vec_rbind
 #' @importFrom withr with_options
 NULL
+
+utils::globalVariables(".x")
