573e697641ef5f944e5ee1c7e9039c5a73f8e828
wlandau <will.landau@gmail.com>
2020-10-28 20:55:20 -0400

    Implement tar_knit_raw()

---
+++ tests/testthat/test-tar_knit_raw.R
@@ -0,0 +1,122 @@
+# Building in a temporary directory with tar_test() seems to break
+# fs::path_rel() on the GitHub Actions Windows check job,
+# and I am not sure why.
+test_that("tar_knit_raw() works", suppressMessages({
+  on.exit(unlink(c("_targets*", "report.*"), recursive = TRUE))
+  lines <- c(
+    "---",
+    "title: report",
+    "output_format: html_document",
+    "---",
+    "",
+    "```{r}",
+    "targets::tar_read(data)",
+    "```"
+  )
+  writeLines(lines, "report.Rmd")
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_target(data, data.frame(x = seq_len(26L), y = letters)),
+      tar_knit_raw("report", "report.Rmd", quiet = TRUE)
+    )
+  })
+  # First run.
+  targets::tar_make(callr_function = NULL)
+  expect_equal(sort(targets::tar_progress()$name), sort(c("data", "report")))
+  out <- targets::tar_read(report)
+  # Paths must be relative.
+  expect_equal(out, c("report.md", "report.Rmd"))
+  # Should not rerun the report.
+  targets::tar_make(callr_function = NULL)
+  expect_equal(nrow(targets::tar_progress()), 0L)
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_target(data, data.frame(x = rev(seq_len(26L)), y = letters)),
+      tar_knit_raw("report", "report.Rmd")
+    )
+  })
+  # Should rerun the report.
+  targets::tar_make(callr_function = NULL)
+  expect_equal(sort(targets::tar_progress()$name), sort(c("data", "report")))
+}))
+
+test_that("tar_knit_raw() warns about tar_read_raw()", suppressMessages({
+  on.exit(unlink(c("_targets*", "report.*"), recursive = TRUE))
+  lines <- c(
+    "---",
+    "title: report",
+    "output_format: html_document",
+    "---",
+    "",
+    "```{r}",
+    "targets::tar_read_raw('data')",
+    "```"
+  )
+  writeLines(lines, "report.Rmd")
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_target(data, data.frame(x = seq_len(26L), y = letters)),
+      tar_knit_raw("report", "report.Rmd", quiet = TRUE)
+    )
+  })
+  expect_warning(
+    targets::tar_make(callr_function = NULL),
+    class = "condition_validate"
+  )
+}))
+
+test_that("tar_knit_raw() warns about tar_load_raw()", suppressMessages({
+  on.exit(unlink(c("_targets*", "report.*"), recursive = TRUE))
+  lines <- c(
+    "---",
+    "title: report",
+    "output_format: html_document",
+    "---",
+    "",
+    "```{r}",
+    "envir <- new.env(parent = emptyenv())",
+    "targets::tar_load_raw('data', envir = envir)",
+    "```"
+  )
+  writeLines(lines, "report.Rmd")
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_target(data, data.frame(x = seq_len(26L), y = letters)),
+      tar_knit_raw("report", "report.Rmd", quiet = TRUE)
+    )
+  })
+  expect_warning(
+    targets::tar_make(callr_function = NULL),
+    class = "condition_validate"
+  )
+}))
+
+tar_test("tar_knit_raw() on a nested report still runs from the project root", {
+  lines <- c(
+    "---",
+    "title: report",
+    "output_format: html_document",
+    "---",
+    "",
+    "```{r}",
+    "file.create(\"here\")",
+    "```"
+  )
+  dir.create("out")
+  writeLines(lines, file.path("out", "report.Rmd"))
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_knit_raw("report", file.path("out", "report.Rmd"))
+    )
+  })
+  expect_false(file.exists("here"))
+  expect_false(file.exists(file.path("out", "here")))
+  targets::tar_make(callr_function = NULL)
+  expect_true(file.exists("here"))
+  expect_false(file.exists(file.path("out", "here")))
+})
