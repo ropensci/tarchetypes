a23e1c1be6c6907337cba405b89da8bc7f41204b
wlandau <will.landau@gmail.com>
2020-12-06 09:52:30 -0500

    Add tar_files_raw()

--- R/tar_files.R
+++ R/tar_files.R
@@ -29,10 +29,14 @@
 #' @examples
 #' if (identical(Sys.getenv("TARCHETYPES_LONG_EXAMPLES"), "true")) {
 #' # Without loss of generality,
-#' # tar_files(your_target, c("a.txt", "b.txt"))
+#' # tar_files(your_target, fn_that_returns_paths(c("a.txt", "b.txt")))
 #' # is equivalent to:
 #' # list(
-#' #   tar_target(your_target_files, c("a.txt", "b.txt")),
+#' #   tar_target(
+#' #     your_target_files,
+#' #     fn_that_returns_paths(c("a.txt", "b.txt")),
+#' #     cue = tar_cue(mode = "always")
+#' #   ),
 #' #   tar_target(
 #' #     your_target,
 #' #     your_target_files,
@@ -78,16 +82,12 @@ tar_files <- function(
   cue = targets::tar_option_get("cue")
 ) {
   name <- deparse_language(substitute(name))
-  name_files <- paste0(name, "_files")
   envir <- tar_option_get("envir")
   command <- tidy_eval(substitute(command), envir, tidy_eval)
-  files <- tidy_eval(substitute(files), envir, tidy_eval)
   format <- match.arg(format)
   tar_files_raw(
     name = name,
-    name_files = name_files,
     command = command,
-    files = files,
     packages = packages,
     library = library,
     format = format,
@@ -104,25 +104,100 @@ tar_files <- function(
   )
 }
 
+#' @title Easy branching over dynamic files or urls (raw version).
+#' @description Shorthand for a pattern that correctly
+#'   branches over files or urls.
+#' @details `tar_files_raw()` is similar to [tar_files()]
+#'   except the `name` argument must be a character string
+#'   and `command` must be a language object.
+#'
+#'   `tar_files_raw()` creates a pair of targets, one upstream
+#'   and one downstream. The upstream target does some work
+#'   and returns some file paths, and the downstream
+#'   target is a pattern that applies `format = "file"`
+#'   or `format = "url"`.
+#'   This is the correct way to dynamically
+#'   iterate over file/url targets. It makes sure any downstream patterns
+#'   only rerun some of their branches if the files/urls change.
+#'   For more information, visit
+#'   <https://github.com/wlandau/targets/issues/136> and
+#'   <https://github.com/ropensci/drake/issues/1302>.
+#' @export
+#' @inheritParams targets::tar_target
+#' @return A list of two targets, one upstream and one downstream.
+#'   The upstream one does some work and returns some file paths,
+#'   and the downstream target is a pattern that applies `format = "file"`
+#'   or `format = "url"`.
+#' @param tidy_eval Whether to invoke tidy evaluation
+#'   (e.g. the `!!` operator from `rlang`) as soon as the target is defined
+#'   (before `tar_make()`). Applies to the `command` argument.
+#' @param format Character, either `"file"` or `"url"`. See the `format`
+#'   argument of `targets::tar_target()` for details.
+#' @param cue An optional object from `tar_cue()`
+#'   to customize the rules that decide whether the target is up to date.
+#'   Only applies to the downstream target. The upstream target always runs.
+#' @examples
+#' if (identical(Sys.getenv("TARCHETYPES_LONG_EXAMPLES"), "true")) {
+#' # Without loss of generality,
+#' # tar_files_raw(
+#' #   "your_target",
+#' #   quote(fn_that_returns_paths(c("a.txt", "b.txt")))
+#' # )
+#' # is equivalent to:
+#' # list(
+#' #   tar_target(
+#' #     your_target_files,
+#' #     fn_that_returns_paths(c("a.txt", "b.txt")),
+#' #     cue = tar_cue(mode = "always")
+#' #   ),
+#' #   tar_target(
+#' #     your_target,
+#' #     your_target_files,
+#' #     pattern = map(your_target_files),
+#' #     format = "file",
+#' #     deployment = "main",
+#' #     storage = "main",
+#' #     retrieval = "main"
+#' #   )
+#' # )
+#' # Try it out.
+#' targets::tar_dir({
+#' file.create(c("a.txt", "b.txt"))
+#' targets::tar_script({
+#'   tar_pipeline(
+#'     tarchetypes::tar_files_raw("x", quote(c("a.txt", "b.txt")))
+#'   )
+#' })
+#' targets::tar_make()
+#' # Should be up to date now.
+#' targets::tar_make()
+#' # If we change one file, `tar_make()` will only rerun one branch.
+#' writeLines("a", "a.txt")
+#' targets::tar_make()
+#' })
+#' }
 tar_files_raw <- function(
   name,
-  name_files,
   command,
-  files,
-  packages,
-  library,
-  format,
-  iteration,
-  error,
-  memory,
-  garbage_collection,
-  deployment,
-  priority,
-  resources,
-  storage,
-  retrieval,
-  cue
+  packages = targets::tar_option_get("packages"),
+  library = targets::tar_option_get("library"),
+  format = c("file", "url"),
+  iteration = targets::tar_option_get("iteration"),
+  error = targets::tar_option_get("error"),
+  memory = targets::tar_option_get("memory"),
+  garbage_collection = targets::tar_option_get("garbage_collection"),
+  deployment = targets::tar_option_get("deployment"),
+  priority = targets::tar_option_get("priority"),
+  resources = targets::tar_option_get("resources"),
+  storage = targets::tar_option_get("storage"),
+  retrieval = targets::tar_option_get("retrieval"),
+  cue = targets::tar_option_get("cue")
 ) {
+  assert_chr(name, "name must be a character.")
+  assert_scalar(name, "name must have length 1.")
+  assert_lang(command, "command must be a language object.")
+  name_files <- paste0(name, "_files")
+  format <- match.arg(format)
   upstream <- tar_target_raw(
     name = name_files,
     command = command,
