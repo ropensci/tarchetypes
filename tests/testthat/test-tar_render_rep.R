09b112d45b026f9ab096106519c62c09b214e376
wlandau <will.landau@gmail.com>
2020-12-12 20:27:12 -0500

    Add a test

--- tests/testthat/test-tar_render_rep.R
+++ tests/testthat/test-tar_render_rep.R
@@ -30,3 +30,35 @@ tar_test("tar_render_rep() manifest", {
   expect_equal(sum(is.na(out$pattern)), 2L)
   expect_equal(out$pattern[out$name == "report"], "map(report_params)")
 })
+
+tar_test("tar_render_rep() graph", {
+  lines <- c(
+    "---",
+    "title: report",
+    "output_format: html_document",
+    "params:",
+    "  par: \"default value\"",
+    "---",
+    "",
+    "```{r}",
+    "print(params$par)",
+    "print(tar_read(x))",
+    "```"
+  )
+  writeLines(lines, "report.Rmd")
+  targets::tar_script({
+    library(tarchetypes)
+    tar_pipeline(
+      tar_target(x, "value_of_x"),
+      tar_render_rep(
+        report,
+        "report.Rmd",
+        params = quote(tibble(par = c("par_value_1", "par_value_2")))
+      )
+    )
+  })
+  out <- targets::tar_network(callr_function = NULL)$edges
+  expect_equal(nrow(out), 2L)
+  expect_true(any(out$from == "report_params" & out$to == "report"))
+  expect_true(any(out$from == "x" & out$to == "report"))
+})
