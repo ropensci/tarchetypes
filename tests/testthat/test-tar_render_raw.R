7c25665bd2475829a5b3d93280b897ad25de3072
wlandau <will.landau@gmail.com>
2021-01-18 12:35:16 -0500

    Use basenames

--- tests/testthat/test-tar_render_raw.R
+++ tests/testthat/test-tar_render_raw.R
@@ -21,8 +21,7 @@ targets::tar_test("tar_render_raw() works", {
   suppressMessages(targets::tar_make(callr_function = NULL))
   expect_equal(sort(targets::tar_progress()$name), sort(c("data", "report")))
   out <- targets::tar_read(report)
-  # Paths must be relative.
-  expect_equal(out, c("report.html", "report.Rmd"))
+  expect_equal(basename(out), c("report.html", "report.Rmd"))
   # Should not rerun the report.
   suppressMessages(targets::tar_make(callr_function = NULL))
   expect_equal(nrow(targets::tar_progress()), 0L)
