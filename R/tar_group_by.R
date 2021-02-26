7c97eda7c7b88d0b88ce54b29e6e24f066beddae
wlandau <will.landau@gmail.com>
2021-02-26 16:20:59 -0500

    Finish tar_group_by() code sketch

--- R/tar_group_by.R
+++ R/tar_group_by.R
@@ -17,10 +17,12 @@ tar_group_by <- function(
   retrieval = targets::tar_option_get("retrieval"),
   cue = targets::tar_option_get("cue")
 ) {
+  assert_package("dplyr")
   name <- deparse_language(substitute(name))
   assert_lgl(tidy_eval, "tidy_eval in tar_target() must be logical.")
-  columns <- as.expression(substitute(list(...)))
-  command <- tar_group_by_command(substitute(command), columns, envir, tidy_eval)
+  by <- all.vars(substitute(list(...)), functions = FALSE)
+  assert_nonempty(by, "no columns to group by.")
+  command <- tar_group_by_command(substitute(command), by, envir, tidy_eval)
   targets::tar_target_raw(
     name = name,
     command = command,
@@ -40,10 +42,21 @@ tar_group_by <- function(
   )
 }
 
-tar_group_by_command <- function(command, columns, envir, tidy_eval) {
+tar_group_by_command <- function(command, by, envir, tidy_eval) {
   envir <- targets::tar_option_get("envir")
   assert_envir(envir)
   command <- tidy_eval(command, envir, tidy_eval)
   fun <- call_ns("tarchetypes", "tar_group_by_run")
-  as.call(list(fun, command = command, columns = columns))
+  as.call(list(fun, data = command, by = by))
+}
+
+tar_group_by_run <- function(data, by) {
+  assert_df(data, "tar_group_by() output must be a data frame.")
+  assert_in(by, colnames(data), "tar_group_by() columns must be in data.")
+  expr <- quote(dplyr::group_by(data, !!!by_syms))
+  by_syms <- rlang::syms(by)
+  envir <- environment()
+  expr <- tidy_eval(expr, envir = envir, TRUE)
+  grouped <- eval(expr, envir = envir)
+  targets::tar_group(grouped)
 }
