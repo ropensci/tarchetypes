b79e5ec7c3663c718feb606f28a8b8a2ef54dd80
wlandau <will.landau@gmail.com>
2021-05-03 09:21:50 -0400

    Add initial tests

--- tests/testthat/test-tar_hook_before.R
+++ tests/testthat/test-tar_hook_before.R
@@ -15,4 +15,30 @@ targets::tar_test("tar_hook_before() inserts code", {
     )
   })
   out <- targets::tar_manifest(callr_function = NULL)
+  expect_equal(sort(out$name), sort(c("x1", "x2", "x3", "y1")))
+  expect_true(all(grepl("Running hook", out$command)))
+})
+
+targets::tar_test("tar_hook_before() with tidyselect", {
+  targets::tar_script({
+    targets <- list(
+      list(
+        targets::tar_target(x1, task1()),
+        targets::tar_target(x2, task2(x1))
+      ),
+      targets::tar_target(x3, task3(x2)),
+      targets::tar_target(y1, task4(x3))
+    )
+    tarchetypes::tar_hook_before(
+      targets = targets,
+      hook = print("Running hook."),
+      names = starts_with("x")
+    )
+  })
+  out <- targets::tar_manifest(callr_function = NULL)
+  expect_equal(sort(out$name), sort(c("x1", "x2", "x3", "y1")))
+  expect_equal(
+    grepl("Running hook", out$command),
+    grepl("^x", out$name)
+  )
 })
