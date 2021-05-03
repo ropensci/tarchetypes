313ada91d2efbe86ad49bd7befb180b3d4b6fed7
wlandau <will.landau@gmail.com>
2021-05-03 10:23:12 -0400

    Add another test

--- R/tar_hook_before.R
+++ R/tar_hook_before.R
@@ -1,8 +1,12 @@
 #' @title First hook
 #' @export
 #' @family hooks
-#' @description Prepend R code to the commands of multiple targets
-#' @return A list of target objects.
+#' @description Prepend R code to the commands of multiple targets.
+#' @details Hook functions modify target objects in place and invisibly
+#'   return `NULL`. Users are responsible for declaring the
+#'   final target list at the bottom of `_targets.R`
+#'   so all targets make it into the pipeline.
+#' @return `NULL` (invisibly). The target objects are modified in place.
 #' @inheritSection tar_map Target objects
 #' @param targets A list of target objects.
 #' @param hook R code to insert. When you supply this argument,
@@ -11,6 +15,8 @@
 #' @param names Name of targets in the target list
 #'   to apply the hook. You can supply symbols, a character vector,
 #'   or tidyselect helpers like [starts_with()].
+#'   Targets not included in `names` still remain in the target list,
+#'   but they are not modified because the hook does not apply to them.
 #' @examples
 #' if (identical(Sys.getenv("TAR_LONG_EXAMPLES"), "true")) {
 #' targets::tar_dir({ # tar_dir() runs code from a temporary directory.
@@ -24,11 +30,13 @@
 #'     targets::tar_target(x3, task3(x2)),
 #'     targets::tar_target(y1, task4(x3))
 #'   )
+#'   # Modifies target objects in place and invisibly returns NULL:
 #'   tarchetypes::tar_hook_before(
 #'     targets = targets,
 #'     hook = print("Running hook."),
 #'     names = starts_with("x")
 #'   )
+#'   targets # Return the target list.
 #' })
 #' targets::tar_manifest(fields = command)
 #' })
@@ -43,7 +51,7 @@ tar_hook_before <- function(targets, hook, names = NULL) {
     fun = tar_hook_before_insert,
     hook = hook
   )
-  targets
+  invisible()
 }
 
 tar_hook_before_insert <- function(target, hook) {
--- tests/testthat/test-tar_hook_before.R
+++ tests/testthat/test-tar_hook_before.R
@@ -13,6 +13,7 @@ targets::tar_test("tar_hook_before() inserts code", {
       hook = print("Running hook."),
       names = NULL
     )
+    targets
   })
   out <- targets::tar_manifest(callr_function = NULL)
   expect_equal(sort(out$name), sort(c("x1", "x2", "x3", "y1")))
@@ -34,6 +35,7 @@ targets::tar_test("tar_hook_before() with tidyselect", {
       hook = print("Running hook."),
       names = starts_with("x")
     )
+    targets
   })
   out <- targets::tar_manifest(callr_function = NULL)
   expect_equal(sort(out$name), sort(c("x1", "x2", "x3", "y1")))
@@ -41,4 +43,54 @@ targets::tar_test("tar_hook_before() with tidyselect", {
     grepl("Running hook", out$command),
     grepl("^x", out$name)
   )
+})
+
+targets::tar_test("tar_hook_before() changes internals properly", {
+  x <- targets::tar_target(
+    "a",
+    b,
+    pattern = map(c),
+    format = "file",
+    resources = list(x = 1)
+  )
+  y <- targets::tar_target(
+    "a",
+    b,
+    pattern = map(c),
+    format = "file",
+    resources = list(x = 1)
+  )
+  for (field in c("packages", "library", "deps", "seed", "string", "hash")) {
+    expect_equal(x$command[[field]], y$command[[field]])
+  }
+  for (field in setdiff(names(x$settings), "pattern")) {
+    expect_equal(x$settings[[field]], y$settings[[field]])
+  }
+  expect_equal(deparse(x$settings$pattern), deparse(y$settings$pattern))
+  for (field in names(x$cue)) {
+    expect_equal(x$cue[[field]], y$cue[[field]])
+  }
+  expect_equal(x$store$resources, y$store$resources)
+  # Apply the hook.
+  tar_hook_before(y, f())
+  # Most elements should stay the same
+  for (field in c("packages", "library", "seed")) {
+    expect_equal(x$command[[field]], y$command[[field]])
+  }
+  for (field in setdiff(names(x$settings), "pattern")) {
+    expect_equal(x$settings[[field]], y$settings[[field]])
+  }
+  expect_equal(deparse(x$settings$pattern), deparse(y$settings$pattern))
+  for (field in names(x$cue)) {
+    expect_equal(x$cue[[field]], y$cue[[field]])
+  }
+  expect_equal(x$store$resources, y$store$resources)
+  # Some elements should be different.
+  for (field in c("string", "hash")) {
+    expect_equal(length(y$command[[field]]), 1L)
+    expect_false(x$command[[field]] == y$command[[field]])
+  }
+  expect_true("b" %in% x$command$deps)
+  expect_false("f" %in% x$command$deps)
+  expect_true(all(c("b", "f") %in% y$command$deps))
 })
