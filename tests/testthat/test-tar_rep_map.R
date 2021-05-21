fb2bcd5c9aa1ef58bfd7428d0f4a483325e6db72
wlandau <will.landau@gmail.com>
2021-05-21 09:25:52 -0400

    Add test

--- tests/testthat/test-tar_rep_map.R
+++ tests/testthat/test-tar_rep_map.R
@@ -63,3 +63,58 @@ targets::tar_test("tar_map_reps() manifest", {
   expect_true(grepl("tar_map_reps_run", out$command))
   expect_false(is.na(out$pattern))
 })
+
+targets::tar_test("tar_map_reps() graph", {
+  targets::tar_script({
+    list(
+      targets::tar_target(label, "aggregate"),
+      tarchetypes::tar_rep(
+        data1,
+        data.frame(value = rnorm(2)),
+        batches = 2,
+        reps = 3
+      ),
+      tarchetypes::tar_rep(
+        data2,
+        list(value = rnorm(2)),
+        batches = 2,
+        reps = 3,
+        iteration = "list"
+      ),
+      tarchetypes::tar_map_reps(
+        aggregate1,
+        data.frame(x = label, value = data1$value + data2$value),
+        data1,
+        data2
+      ),
+      tarchetypes::tar_map_reps(
+        aggregate2,
+        list(value = data1$value + data2$value),
+        data1,
+        data2,
+        iteration = "list"
+      ),
+      tarchetypes::tar_map_reps(
+        aggregate3,
+        data.frame(value = aggregate1$value + aggregate2$value),
+        aggregate1,
+        aggregate2
+      )
+    )
+  })
+  out <- targets::tar_network(callr_function = NULL)
+  exp <- tibble::tribble(
+    ~from, ~to,
+    "data1", "aggregate1",
+    "data2", "aggregate1",
+    "label", "aggregate1",
+    "data1", "aggregate2",
+    "data2", "aggregate2",
+    "aggregate1", "aggregate3",
+    "aggregate2", "aggregate3",
+    "data1_batch", "data1",
+    "data2_batch", "data2"
+  )
+  skip_if_not_installed("dplyr")
+  expect_equal(dplyr::arrange(out$edges, from), dplyr::arrange(exp, from))
+})
