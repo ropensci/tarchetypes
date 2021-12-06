2ec64d6dc0d45c5db0bab688039a84af80996a04
wlandau <will.landau@gmail.com>
2021-12-06 16:36:37 -0500

    Fix docs

--- R/tar_map_rep_raw.R
+++ R/tar_map_rep_raw.R
@@ -1,4 +1,5 @@
-#' @title Batched replication within static branches for data frames (raw version).
+#' @title Batched replication within static branches for data frames
+#'   (raw version).
 #' @export
 #' @family branching
 #' @description Define targets for batched replication
@@ -21,7 +22,8 @@
 #' @param combine Logical of length 1, whether to statically combine
 #'   all the results into a single target downstream.
 #' @inheritSection tar_map Target objects
-#' @inheritParams tar_row_rep
+#' @inheritParams tar_map_rep
+#' @inheritParams targets::tar_target
 #' @examples
 #' if (identical(Sys.getenv("TAR_LONG_EXAMPLES"), "true")) {
 #' targets::tar_dir({ # tar_dir() runs code from a temporary directory.
