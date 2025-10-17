ab1c60024b0dad25628eafc3aee28bb1afd5e18b
wlandau <will.landau.oss@gmail.com>
2025-10-17 16:12:50 -0400

    manual format

--- R/tar_translate.R
+++ R/tar_translate.R
@@ -28,9 +28,9 @@
 #'       tar_target()
 #'
 #'     plot <- ggplot(data) +
-#'           geom_point(aes(x = Temp, y = Ozone)) +
-#'           geom_abline(intercept = model[1], slope = model[2]) +
-#'           theme_gray(24)
+#'       geom_point(aes(x = Temp, y = Ozone)) +
+#'       geom_abline(intercept = model[1], slope = model[2]) +
+#'       theme_gray(24)
 #'   }, memory = "transient")
 #' })
 #' targets::tar_make()
